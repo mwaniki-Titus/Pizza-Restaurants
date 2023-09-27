a6464a96c49a49788ccbce021d334ae06583e8e0
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-09-27 16:36:21 +0300

    requirements update

--- api/app.py
+++ api/app.py
@@ -1,17 +1,19 @@
 from flask import Flask
+from flask_restful import Api
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api
 
-
-from .models import db  
+from .models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurant.db'
-db = SQLAlchemy(app)
-api = Api(app)
+
+
+db.init_app(app)
 migrate = Migrate(app, db)
 
+
 api = Api(app)
 
-from api import routes 
+from api import routes
+
